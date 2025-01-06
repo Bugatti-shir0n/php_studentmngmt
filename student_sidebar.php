8e33fa71ce794b66ad4981eb03fd5083e2f632ac
Lynet_Dev <shirulynn8@gmail.com>
2025-01-06 11:09:55 +0300

    Add files via upload

---
+++ student_sidebar.php
@@ -0,0 +1,26 @@
+<header class="header">
+        <a href="">Student Dashboard</a>
+
+        <div class="logout">
+            <a href="logout.php" class="btn btn-primary">Logout</a>
+        </div>
+    </header>
+
+    <aside>
+        <ul>
+            <li>
+                <a href="student_profile.php">My Profile</a>
+            </li>
+            <li>
+                <a href="">Courses</a>
+            </li>
+            <li>
+                <a href="">Results</a>
+            </li>
+            <li>
+                <a href="">View Calendar</a>
+            </li>
+
+        </ul>
+
+    </aside>
